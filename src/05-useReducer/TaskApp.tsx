22dfe41bae92c782118aab3eef9964f4161a1508
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2026-08-11 14:28:00 +0200

    feat: agregar lógica para las funciones de agregar, actulizar y eliminar ToDos

--- src/05-useReducer/TaskApp.tsx
+++ src/05-useReducer/TaskApp.tsx
@@ -18,19 +18,42 @@ export const TasksApp = () => {
   const [inputValue, setInputValue] = useState("");
 
   const addTodo = () => {
-    console.log("Agregar tarea", inputValue);
+    if (inputValue.length === 0) return;
+
+    // Será de tipo Todo (interface)
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: inputValue.trim(),
+      completed: false,
+    };
+
+    // Agregamos el nuevo ToDo al arreglo de ToDos actual
+    setTodos([...todos, newTodo]);
+
+    //setTodos((prev) => [...prev, newTodo]);
   };
 
   const toggleTodo = (id: number) => {
-    console.log("Cambiar de true a false", id);
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        // Me retorna todos los valores del ToDo actual y el valor opuesto que tiene completed
+        return { ...todo, completed: !todo.completed };
+      }
+      return todo;
+    });
+
+    setTodos(updatedTodos);
   };
 
   const deleteTodo = (id: number) => {
-    console.log("Eliminar tarea", id);
+    const updatedTodos = todos.filter((todo) => todo.id != id);
+    setTodos(updatedTodos);
   };
 
   const handleKeyPress = (e: React.KeyboardEvent) => {
-    console.log("Presiono enter");
+    if (e.key === "Enter") {
+      addTodo();
+    }
   };
 
   const completedCount = todos.filter((todo) => todo.completed).length;
