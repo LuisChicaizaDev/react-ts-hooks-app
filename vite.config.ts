87b395a49b2b584960405697a6432dad6495ea71
Luis <161586660+luischicaizaDev@users.noreply.github.com>
2026-08-13 14:35:08 +0200

    feat: agregar el componente de juego ScrambleWords e integrar canvas-confetti

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import path from "path";
+import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import babel from "@rolldown/plugin-babel";
 import tailwindcss from "@tailwindcss/vite";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
